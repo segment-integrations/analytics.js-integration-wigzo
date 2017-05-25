11ea7ac206059cc4cf6f6d72649c585e93758e02
ashishpal06 <ashishpal06@gmail.com>
2017-05-26 01:47:40 +0530

    added support for sending mandatory events

--- test/index.test.js
+++ test/index.test.js
@@ -84,20 +84,61 @@ describe('Wigzo', function() {
       });
 
       it('should call index', function() {
-        analytics.page();
-        analytics.called(window.wigzo.index);
+        var productData = {
+          product_id: '40',
+          canonicalUrl : 'https://snoopy.wigzopush.com/index.php?route=product/product&amp;product_id=40',
+          imageUrl : 'https://snoopy.wigzopush.com/image/cache/catalog/demo/iphone_1-228x228.jpg',
+          category: 'Mobile Phones',
+          description: 'iPhone is a revolutionary new mobile phone that allows you',
+          language: 'en',
+          name: 'iPhone',
+          brand: 'Apply',
+          price: 18.99,
+          currency: 'usd'
+        };
+
+        var wigzoProduct = {
+          productId: productData.product_id,
+          title: productData.name,
+          price : productData.currency + ' ' + productData.price,
+          category: productData.category,
+          image : productData.imageUrl,
+          canonicalUrl : productData.canonicalUrl,
+          description: productData.description,
+          language: productData.language
+        };
+
+        analytics.track('Product Clicked', productData);
+        analytics.called(window.wigzo.index, wigzoProduct);
       });
 
-      it('should pass page name and default properties via page', function() {
-        analytics.page('Name');
-        analytics.called(window.wigzo.index, {
-          title: document.title,
-          url: window.location.href,
-          name: 'Name',
-          path: window.location.pathname,
-          referrer: document.referrer,
-          search: window.location.search
-        });
+      it('should call index', function() {
+        var productData = {
+          product_id: '40',
+          canonicalUrl : 'https://snoopy.wigzopush.com/index.php?route=product/product&amp;product_id=40',
+          imageUrl : 'https://snoopy.wigzopush.com/image/cache/catalog/demo/iphone_1-228x228.jpg',
+          category: 'Mobile Phones',
+          description: 'iPhone is a revolutionary new mobile phone that allows you',
+          language: 'en',
+          name: 'iPhone',
+          brand: 'Apply',
+          price: 18.99,
+          currency: 'usd'
+        };
+
+        var wigzoProduct = {
+          productId: productData.product_id,
+          title: productData.name,
+          price : productData.currency + ' ' + productData.price,
+          category: productData.category,
+          image : productData.imageUrl,
+          canonicalUrl : productData.canonicalUrl,
+          description: productData.description,
+          language: productData.language
+        };
+
+        analytics.track('Product Viewed', productData);
+        analytics.called(window.wigzo.index, wigzoProduct);
       });
     });
 
@@ -117,12 +158,15 @@ describe('Wigzo', function() {
           email: 'ashish@wigzo.com',
           phone: '1234567890'
         };
-        analytics.identify(user);
-        analytics.called(window.wigzo.identify, {
+
+        var wigzoUser = {
           fullName: user.name,
           email: user.email,
           phone: user.phone
-        });
+        };
+        
+        analytics.identify(user);
+        analytics.called(window.wigzo.identify, wigzoUser);
       });
 
       it('should send an id and traits', function() {
@@ -160,6 +204,144 @@ describe('Wigzo', function() {
 
         analytics.track('event', eventData);
         analytics.called(window.wigzo.track, 'event', eventData);
+      });
+
+      it('should send addtocart', function() {
+        var eventData = {
+          product_id: '507f1f77bcf86cd799439011'
+        };
+        analytics.track('Product Added', eventData);
+        analytics.called(window.wigzo.track, 'addtocart', eventData.product_id);
+      });
+
+      it('should send Wishlist Product Added to Cart', function() {
+        var eventData = {
+          product_id: '507f1f77bcf86cd799439011'
+        };
+        analytics.track('Product Added to Cart from Wishlist', eventData);
+        analytics.called(window.wigzo.track, 'addtocart', eventData.product_id);
+      });
+
+      it('should not send addtocart without product_id', function() {
+        var eventData = {
+          name: 'Monopoly: 3rd Edition',
+          brand: 'Hasbro'
+        };
+        analytics.track('Product Added', eventData);
+        analytics.didNotCall(window.wigzo.track, 'addtocart');
+      });
+
+      it('should send wishlist', function() {
+        var eventData = {
+          product_id: '507f1f77bcf86cd799439011'
+        };
+        analytics.track('Product Added to Wishlist', eventData);
+        analytics.called(window.wigzo.track, 'wishlist', eventData.product_id);
+      });
+
+      it('should not send wishlist without product_id', function() {
+        var eventData = {
+          name: 'Monopoly: 3rd Edition',
+          brand: 'Hasbro'
+        };
+        analytics.track('Product Added to Wishlist', eventData);
+        analytics.didNotCall(window.wigzo.track, 'wishlist');
+      });
+
+      it('should send search', function() {
+        var eventData = {
+          query: 'blue hotpants'
+        };
+
+        analytics.track('Products Searched', eventData);
+        analytics.called(window.wigzo.track, 'search', eventData.query);
+      });
+
+      it('should not send search without query', function() {
+        var eventData = {
+          text: 'blue hotpants'
+        };
+
+        analytics.track('Products Searched', eventData);
+        analytics.didNotCall(window.wigzo.track, 'search');
+      });
+
+      it('should send product removed', function() {
+        var eventData = {
+          product_id: '507f1f77bcf86cd799439011',
+          sku: 'G-32'
+        };
+
+        analytics.track('Product Removed', eventData);
+        analytics.called(window.wigzo.track, 'removedfromcart', eventData.product_id);
+      });
+
+      it('should not send product removed without product_id', function() {
+        var eventData = {
+          name: 'Monopoly: 3rd Edition',
+          sku: 'G-32'
+        };
+
+        analytics.track('Product Removed', eventData);
+        analytics.didNotCall(window.wigzo.track, 'removedfromcart');
+      });
+
+      it('should send product reviewed', function() {
+        var eventData = {
+          product_id: '507f1f77bcf86cd799439011',
+          review_id: 'kdfjrj39fj39jf3',
+          review_body: 'I love this product',
+          rating: '5'
+        };
+
+        analytics.track('Product Reviewed', eventData);
+        analytics.called(window.wigzo.track, 'review', eventData);
+      });
+
+      it('should send checkout started', function() {
+        var productIdList = ['507f1f77bcf86cd799439011', '505bd76785ebb509fc183733'];
+        var eventData = {
+          products: [
+            { product_id: productIdList[0] },
+            { product_id: productIdList[1] }
+          ]
+        };
+
+        analytics.track('Checkout Started', eventData);
+        analytics.called(window.wigzo.track, 'checkoutstarted', productIdList);
+      });
+
+      it('should not send checkout started without product_id', function() {
+        var eventData = {
+          order_id: '50314b8e9bcf000000000000',
+          affiliation: 'Google Store'
+        };
+
+        analytics.track('Checkout Started', eventData);
+        analytics.didNotCall(window.wigzo.track, 'checkoutstarted');
+      });
+
+      it('should send order completed', function() {
+        var productIdList = ['507f1f77bcf86cd799439011', '505bd76785ebb509fc183733'];
+        var eventData = {
+          products: [
+            { product_id: productIdList[0] },
+            { product_id: productIdList[1] }
+          ]
+        };
+
+        analytics.track('Order Completed', eventData);
+        analytics.called(window.wigzo.track, 'buy', productIdList);
+      });
+
+      it('should not send order completed without product_id', function() {
+        var eventData = {
+          order_id: '50314b8e9bcf000000000000',
+          affiliation: 'Google Store'
+        };
+
+        analytics.track('Order Completed', eventData);
+        analytics.didNotCall(window.wigzo.track, 'buy');
       });
     });
   });
